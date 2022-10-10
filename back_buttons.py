8760ea820f9539500522b4105ec8a06321f3faed
ITStudent <ivanu1706@gmail.com>
2022-10-10 18:11:16 +1100

    Красивый и грамотный бот2

--- back_buttons.py
+++ back_buttons.py
@@ -2,10 +2,7 @@ from aiogram.types import ReplyKeyboardRemove, \
     ReplyKeyboardMarkup, KeyboardButton, \
     InlineKeyboardMarkup, InlineKeyboardButton
 
-menu_kb = ReplyKeyboardMarkup(resize_keyboard=True)
-btn1 = KeyboardButton('Menu 1')
-btn2 = KeyboardButton('2 Menu')
-btn3 = KeyboardButton('Me 3 nu')
-btn4 = KeyboardButton('nu 4 Me')
-menu_kb.row(btn1, btn2)
-menu_kb.row(btn3, btn4)
+inline_kb1 = InlineKeyboardMarkup()
+inline_btn_1 = InlineKeyboardButton('ураа', callback_data='button_ura')
+inline_btn_2 = InlineKeyboardButton('ааару', callback_data='button_aru')
+inline_kb1.row(inline_btn_1, inline_btn_2)
--- back_end.py
+++ back_end.py
@@ -1,12 +1,16 @@
 from aiogram import Bot, Dispatcher, types
-from back_buttons import menu_kb
+from back_buttons import inline_kb1
 
 async def handler_start(message: types.Message):
     await message.answer('Стандартный текст')
 
 
 async def handler_hello(message: types.Message):
-    await message.answer('Hello there', reply_markup=menu_kb)
+    await message.answer('Hello there', reply_markup=inline_kb1)
+
+
+async def handler_btn(callback: types.CallbackQuery):
+    await callback.answer('Победа')
 
 
 async def handler_echo(message: types.Message):
@@ -17,4 +21,5 @@ def register_handler_menu(dp: Dispatcher):
     dp.register_message_handler(handler_start, commands='start')
     dp.register_message_handler(handler_hello, commands='hi')
     dp.register_message_handler(handler_echo, text='echo')
+    dp.register_message_handler(handler_btn, callback='button_ura')
 
