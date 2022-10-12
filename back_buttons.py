1dc70d8a1474c4141f414e566c9307473e59837b
ITStudent <ivanu1706@gmail.com>
2022-10-12 16:26:13 +1100

    Многааа кнопок и менюшек

--- back_buttons.py
+++ back_buttons.py
@@ -3,6 +3,40 @@ from aiogram.types import ReplyKeyboardRemove, \
     InlineKeyboardMarkup, InlineKeyboardButton
 
 inline_kb1 = InlineKeyboardMarkup()
-inline_btn_1 = InlineKeyboardButton('ураа', callback_data='button_ura')
-inline_btn_2 = InlineKeyboardButton('ааару', callback_data='button_aru')
-inline_kb1.row(inline_btn_1, inline_btn_2)
+inline_btn_food = InlineKeyboardButton('Еда', callback_data='button_food')
+inline_btn_drink = InlineKeyboardButton('Напитки', callback_data='button_drink')
+inline_kb1.row(inline_btn_food, inline_btn_drink)
+
+inline_kb2 = InlineKeyboardMarkup()
+inline_btn_1st = InlineKeyboardButton('Первое', callback_data='button_food_1st')
+inline_btn_2nd = InlineKeyboardButton('Второе', callback_data='button_food_2nd')
+inline_btn_snak = InlineKeyboardButton('Закуски', callback_data='button_food_snak')
+inline_btn_desert = InlineKeyboardButton('Десерты', callback_data='button_food_desert')
+inline_btn_back = InlineKeyboardButton('Назад', callback_data='button_f_back')
+inline_kb2.add(inline_btn_1st, inline_btn_2nd, inline_btn_snak, inline_btn_desert, inline_btn_back)
+
+inline_kb3 = InlineKeyboardMarkup()
+inline_btn_hot = InlineKeyboardButton('Горячие', callback_data='button_drink_hot')
+inline_btn_cold = InlineKeyboardButton('Холодные', callback_data='button_drink_cold')
+inline_btn_alk = InlineKeyboardButton('Алкогольные', callback_data='button_drink_alk')
+inline_kb3.add(inline_btn_hot, inline_btn_cold, inline_btn_alk, inline_btn_back)
+
+inline_kb4 = InlineKeyboardMarkup()
+inline_btn_tea = InlineKeyboardButton('Чай', callback_data='button_drink_hot_tea')
+inline_btn_coffe = InlineKeyboardButton('Кофе', callback_data='button_drink_hot_coffe')
+inline_btn_cacao = InlineKeyboardButton('Какао', callback_data='button_drink_hot_cacao')
+inline_btn_back2 = InlineKeyboardButton('Назад', callback_data='button_f_back2')
+inline_kb4.add(inline_btn_tea, inline_btn_coffe, inline_btn_cacao, inline_btn_back2)
+
+inline_kb5 = InlineKeyboardMarkup()
+inline_btn_water = InlineKeyboardButton('Вода', callback_data='button_drink_cold_water')
+inline_btn_juce = InlineKeyboardButton('Сок', callback_data='button_drink_cold_juce')
+inline_btn_fizzy_w = InlineKeyboardButton('Газировка', callback_data='button_drink_cold_fizzy_water')
+inline_kb5.add(inline_btn_water, inline_btn_juce, inline_btn_fizzy_w, inline_btn_back2)
+
+inline_kb6 = InlineKeyboardMarkup()
+inline_btn_pivo = InlineKeyboardButton('Пиво', callback_data='button_drink_alk_pivo')
+inline_btn_vodka = InlineKeyboardButton('Водка', callback_data='button_drink_alk_vodka')
+inline_btn_vino = InlineKeyboardButton('Вино', callback_data='button_drink_alk_vino')
+inline_btn_shamp = InlineKeyboardButton('Шампанское', callback_data='button_drink_alk_shamp')
+inline_kb6.add(inline_btn_pivo, inline_btn_vodka, inline_btn_vino, inline_btn_shamp, inline_btn_back2)
--- back_end.py
+++ back_end.py
@@ -1,25 +1,57 @@
 from aiogram import Bot, Dispatcher, types
-from back_buttons import inline_kb1
+from back_buttons import inline_kb1, inline_kb2, inline_kb3, inline_kb4, inline_kb5, inline_kb6, ReplyKeyboardRemove
 
 async def handler_start(message: types.Message):
-    await message.answer('Стандартный текст')
+    await message.answer('Ну что ж, начнём!\nЧего же ты хочешь?...', reply_markup=inline_kb1)
 
 
 async def handler_hello(message: types.Message):
-    await message.answer('Hello there', reply_markup=inline_kb1)
+    await message.answer('Hello there')
 
 
-async def handler_btn(callback: types.CallbackQuery):
-    await callback.answer('Победа')
+async def handler_btn_food(callback: types.CallbackQuery):
+    await callback.message.edit_text('Выбирай.', reply_markup=inline_kb2)
 
+
+async def handler_btn_drink(callback: types.CallbackQuery):
+    await callback.message.edit_text('Выбирай!', reply_markup=inline_kb3)
+
+
+async def handler_btn_back(callback: types.CallbackQuery):
+    await callback.message.edit_text('Чего же ты хочешь?', reply_markup=inline_kb1)
+
+
+async def handler_btn_drink_hot(callback: types.CallbackQuery):
+    await callback.message.edit_text('Выбирай!', reply_markup=inline_kb4)
+
+
+async def handler_btn_drink_cold(callback: types.CallbackQuery):
+    await callback.message.edit_text('Выбирай!', reply_markup=inline_kb5)
+
+
+async def handler_btn_drink_alk(callback: types.CallbackQuery):
+    await callback.message.edit_text('Выбирай!', reply_markup=inline_kb6)
+
+async def handler_btn_back2(callback: types.CallbackQuery):
+    await callback.message.edit_text('Чего же ты хочешь?', reply_markup=inline_kb3)
 
 async def handler_echo(message: types.Message):
     await message.answer(message.text)
+
+async def cancler(message: types.Message):
+    await message.answer('deleted', reply_markup=ReplyKeyboardRemove())
 
 
 def register_handler_menu(dp: Dispatcher):
     dp.register_message_handler(handler_start, commands='start')
     dp.register_message_handler(handler_hello, commands='hi')
     dp.register_message_handler(handler_echo, text='echo')
-    dp.register_message_handler(handler_btn, callback='button_ura')
+    dp.register_callback_query_handler(handler_btn_food, lambda c: c.data == 'button_food')
+    dp.register_callback_query_handler(handler_btn_drink, lambda c: c.data == 'button_drink')
+    dp.register_callback_query_handler(handler_btn_back, lambda c: c.data == 'button_f_back')
+    dp.register_callback_query_handler(handler_btn_drink_hot, lambda c: c.data == 'button_drink_hot')
+    dp.register_callback_query_handler(handler_btn_drink_cold, lambda c: c.data == 'button_drink_cold')
+    dp.register_callback_query_handler(handler_btn_drink_alk, lambda c: c.data == 'button_drink_alk')
+    dp.register_callback_query_handler(handler_btn_back2, lambda c: c.data == 'button_f_back2')
+    dp.register_message_handler(cancler, commands='del')
 
