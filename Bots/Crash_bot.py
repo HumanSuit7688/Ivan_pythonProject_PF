e612063ed717f34ea552987443174c4cfb4e7a7b
ITStudent <ivanu1706@gmail.com>
2022-10-24 16:50:01 +1100

    Краш с криптой

--- Bots/Crash_bot.py
+++ Bots/Crash_bot.py
@@ -8,22 +8,22 @@ stv = input().lower()
 id = crypta
 crypto = cg.get_price(ids= id , vs_currencies= 'usd')
 price = crypto[id]['usd']
-print(f'Один биткоин = {price} долларов')
-time.sleep(20)
+print(f'One {crypta} = {price} долларов')
+time.sleep(35)
 crypto2 = cg.get_price(ids= id , vs_currencies= 'usd')
-price2 = crypto2
-# большем    бм   бом
+price2 = crypto2[id]['usd']
+# большем
 if "больше" in stv:
     if price2 > price:
         print('Ураа, Вы победили!')
-        print(f'Один биткоин = {price2} долларов')
+        print(f'One {crypta} = {price2} долларов')
     else:
         print("К сожалению вы проиграли")
-        print(f'Один биткоин = {price2} долларов')
+        print(f'One {crypta} = {price2} долларов')
 elif 'меньше' in stv:
     if price2 < price:
         print('Ураа, Вы победили!')
-        print(f'Один биткоин = {price2} долларов')
+        print(f'One {crypta} = {price2} долларов')
     else:
         print("К сожалению вы проиграли")
-        print(f'Один биткоин = {price2} долларов')
+        print(f'One {crypta} = {price2} долларов')
