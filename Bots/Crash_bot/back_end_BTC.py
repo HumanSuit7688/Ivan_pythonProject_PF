aa2e80cb10a10cffa0ff00d087f78df1d6a9cefe
ITStudent <ivanu1706@gmail.com>
2022-11-09 18:08:49 +1100

    Краш с криптой, починил и сделал ставку монетами

--- Bots/Crash_bot/back_end_BTC.py
+++ Bots/Crash_bot/back_end_BTC.py
@@ -1,27 +1,26 @@
-import asyncio
 from pycoingecko import CoinGeckoAPI
 from aiogram.types import ReplyKeyboardRemove, \
     InlineKeyboardButton, InlineKeyboardMarkup, ReplyKeyboardMarkup
-
 cg = CoinGeckoAPI()
-
-def crypto_crash(id, stv, price):
+def crypto_crash(id, stv, price, stv_int):
     crypto2 = cg.get_price(ids=id, vs_currencies='usd')
     price2 = crypto2[id]['usd']
-    prc_for_comp2 = price2 * 100
 
     if "больше" in stv:
         if price2 > price:
             result = 'Ура, вы выиграли!'
+            stv_int = stv_int * 2
         else:
             result = 'К сожалению вы проиграли!'
+            stv_int = 0
     elif 'меньше' in stv:
         if price2 < price:
             result = 'Ура, вы выиграли!'
+            stv_int = stv_int * 2
         else:
             result = 'К сожалению вы проиграли!'
-
-    return result
+            stv_int = 0
+    return result, str(stv_int)
 
 inline_kb_st = InlineKeyboardMarkup()
 inline_btn_more = InlineKeyboardButton('Больше', callback_data='button_more')
@@ -30,6 +29,13 @@ inline_kb_st.row(inline_btn_more, inline_btn_less)
 
 inline_kb_ch = InlineKeyboardMarkup()
 inline_btn_btc = InlineKeyboardButton('Bitcoin', callback_data='button_btc')
-inline_btn_eth = InlineKeyboardButton('Ethereum', callback_data='button_ef')
+inline_btn_eth = InlineKeyboardButton('Ethereum', callback_data='button_eth')
 inline_btn_bnb = InlineKeyboardButton('BNB', callback_data='button_bnb')
 inline_kb_ch.row(inline_btn_btc, inline_btn_eth, inline_btn_bnb)
+
+inline_kb_st_int = InlineKeyboardMarkup()
+inline_btn_100c = InlineKeyboardButton('100 монет', callback_data='button_100c')
+inline_btn_200c = InlineKeyboardButton('200 монет', callback_data='button_200c')
+inline_btn_500c = InlineKeyboardButton('500 монет', callback_data='button_500c')
+inline_btn_1000c = InlineKeyboardButton('1000 монет', callback_data='button_1000c')
+inline_kb_st_int.row(inline_btn_100c, inline_btn_200c, inline_btn_500c, inline_btn_1000c)
--- Bots/Crash_bot/bot_main.py
+++ Bots/Crash_bot/bot_main.py
@@ -2,16 +2,24 @@ import asyncio
 from aiogram import types, Bot, Dispatcher
 from aiogram.utils import executor
 from Bots.config import TOKEN
-from back_end_BTC import crypto_crash, inline_kb_st, inline_kb_ch
+from back_end_BTC import crypto_crash, inline_kb_st, inline_kb_ch, inline_kb_st_int
 from pycoingecko import CoinGeckoAPI
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot)
 cg = CoinGeckoAPI()
+
+
 @dp.message_handler(commands='start')
 async def start(message: types.Message):
-    await message.answer('Привет, Выбирай криптовалюту и делай ставку!', reply_markup=inline_kb_ch)
+    await message.answer('Привет, Хочешь сыграть в безобидный симулятор казино?\n'
+                         'Тогда выбирай криптовалюту и делай ставку!', reply_markup=inline_kb_ch)
+
+
 price = 0
 crypto_id = ''
+stv = ''
+
+
 @dp.callback_query_handler(lambda c: c.data == 'button_btc')
 async def change_btc(callback_query: types.CallbackQuery):
     global crypto_id
@@ -20,7 +28,8 @@ async def change_btc(callback_query: types.CallbackQuery):
     crypto = cg.get_price(ids=crypto_id, vs_currencies='usd')
     price = crypto[crypto_id]['usd']
     print(price)
-    await callback_query.message.edit_text('Теперь сделайте ставку', reply_markup=inline_kb_st)
+    await callback_query.message.edit_text('Отлично, теперь сделай ставку', reply_markup=inline_kb_st)
+
 
 @dp.callback_query_handler(lambda c: c.data == 'button_eth')
 async def change_eth(callback_query: types.CallbackQuery):
@@ -29,7 +38,8 @@ async def change_eth(callback_query: types.CallbackQuery):
     crypto_id = 'ethereum'
     crypto = cg.get_price(ids=crypto_id, vs_currencies='usd')
     price = crypto[crypto_id]['usd']
-    await callback_query.message.edit_text('Теперь сделайте ставку', reply_markup=inline_kb_st)
+    await callback_query.message.edit_text('Найс, теперь делай ставку', reply_markup=inline_kb_st)
+
 
 @dp.callback_query_handler(lambda c: c.data == 'button_bnb')
 async def change_bnb(callback_query: types.CallbackQuery):
@@ -38,21 +48,58 @@ async def change_bnb(callback_query: types.CallbackQuery):
     crypto_id = 'binancecoin'
     crypto = cg.get_price(ids=crypto_id, vs_currencies='usd')
     price = crypto[crypto_id]['usd']
-    await callback_query.message.edit_text('Теперь сделайте ставку', reply_markup=inline_kb_st)
+    await callback_query.message.edit_text('Хорош, теперь сделай ставку', reply_markup=inline_kb_st)
+
 
 @dp.callback_query_handler(lambda c: c.data == 'button_more')
 async def button_more(callback_query: types.CallbackQuery):
+    global stv
     print(callback_query.id)
-    await callback_query.message.edit_text("Пожалуйста подождите 20 секунд")
-    await asyncio.sleep(20)
-    await callback_query.message.edit_text(crypto_crash(crypto_id, 'больше', price))
+    stv = 'больше'
+    await callback_query.message.edit_text("И последнее, выбери размер твоей ставки", reply_markup=inline_kb_st_int)
+
 
 @dp.callback_query_handler(lambda c: c.data == 'button_less')
 async def button_more(callback_query: types.CallbackQuery):
-    await callback_query.message.edit_text("Пожалуйста подождите 20 секунд")
-    await asyncio.sleep(20)
-    await callback_query.message.edit_text(crypto_crash(crypto_id, 'меньше', price))
+    global stv
+    stv = 'меньше'
+    await callback_query.message.edit_text("И последнее, выбери размер твоей ставки", reply_markup=inline_kb_st_int)
 
+
+@dp.callback_query_handler(lambda c: c.data == 'button_100c')
+async def button_st_100c(callback_query: types.CallbackQuery):
+    global stv
+    await callback_query.message.edit_text("Супер, подожди минутку и увидишь результат")
+    await asyncio.sleep(60)
+    await callback_query.message.edit_text(crypto_crash(crypto_id, stv, price, 100)[0])
+    await callback_query.message.answer("Ваш баланс: " + crypto_crash(crypto_id, stv, price, 100)[1] + "монет")
+
+
+@dp.callback_query_handler(lambda c: c.data == 'button_200c')
+async def button_st_100c(callback_query: types.CallbackQuery):
+    global stv
+    await callback_query.message.edit_text("Супер, подожди минутку и увидишь результат")
+    await asyncio.sleep(60)
+    await callback_query.message.edit_text(crypto_crash(crypto_id, stv, price, 200)[0])
+    await callback_query.message.answer("Ваш баланс: " + crypto_crash(crypto_id, stv, price, 200)[1] + " монет")
+
+
+@dp.callback_query_handler(lambda c: c.data == 'button_500c')
+async def button_st_100c(callback_query: types.CallbackQuery):
+    global stv
+    await callback_query.message.edit_text("Супер, подожди минутку и увидишь результат")
+    await asyncio.sleep(60)
+    await callback_query.message.edit_text(crypto_crash(crypto_id, stv, price, 500)[0])
+    await callback_query.message.answer("Ваш баланс: " + crypto_crash(crypto_id, stv, price, 500)[1] + "монет")
+
+
+@dp.callback_query_handler(lambda c: c.data == 'button_1000c')
+async def button_st_100c(callback_query: types.CallbackQuery):
+    global stv
+    await callback_query.message.edit_text("Супер, подожди минутку и увидишь результат")
+    await asyncio.sleep(60)
+    await callback_query.message.edit_text(crypto_crash(crypto_id, stv, price, 1000)[0])
+    await callback_query.message.answer("Ваш баланс: " + crypto_crash(crypto_id, stv, price, 1000)[1] + "монет")
 
 
 if __name__ == '__main__':
